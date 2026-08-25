25518962057a0550a222baa196c9a2601788bba5
Кира <Kirakuleschova@gmail.com>
2026-08-25 14:43:03 +0300

    Заполнила класс TimeSlot

--- ScheduleGenerator/Model/TimeSlot.cs
+++ ScheduleGenerator/Model/TimeSlot.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ScheduleGenerator.Model
+namespace ScheduleGenerator
 {
-    internal class TimeSlot
+    public class TimeSlot
     {
+
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public TimeSpan BreakDuration { get; set; }
+
+
+        // Для удобства отображения в таблице
+        public override string ToString()
+        {
+            return $"{StartTime.ToString(@"hh\:mm")} - {EndTime.ToString(@"hh\:mm")}";
+        }
     }
 }
