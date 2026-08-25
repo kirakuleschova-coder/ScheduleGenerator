973b8629a5eac6f307b9aef427276511a2b51bbd
Кира <Kirakuleschova@gmail.com>
2026-08-25 16:03:19 +0300

    Отредактировала форму, добавила цвета. Работа работает. Всё готово

--- ScheduleGenerator/Form1.Designer.cs
+++ ScheduleGenerator/Form1.Designer.cs
@@ -46,97 +46,118 @@
             // 
             // cmbSpecialty
             // 
+            this.cmbSpecialty.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cmbSpecialty.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.cmbSpecialty.FormattingEnabled = true;
             this.cmbSpecialty.Location = new System.Drawing.Point(12, 12);
             this.cmbSpecialty.Name = "cmbSpecialty";
-            this.cmbSpecialty.Size = new System.Drawing.Size(121, 21);
+            this.cmbSpecialty.Size = new System.Drawing.Size(121, 24);
             this.cmbSpecialty.TabIndex = 0;
             // 
             // cmbGroup
             // 
+            this.cmbGroup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cmbGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.cmbGroup.FormattingEnabled = true;
             this.cmbGroup.Location = new System.Drawing.Point(196, 12);
             this.cmbGroup.Name = "cmbGroup";
-            this.cmbGroup.Size = new System.Drawing.Size(121, 21);
+            this.cmbGroup.Size = new System.Drawing.Size(121, 24);
             this.cmbGroup.TabIndex = 1;
             // 
             // cmbTeacher
             // 
+            this.cmbTeacher.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cmbTeacher.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.cmbTeacher.FormattingEnabled = true;
             this.cmbTeacher.Location = new System.Drawing.Point(370, 12);
             this.cmbTeacher.Name = "cmbTeacher";
-            this.cmbTeacher.Size = new System.Drawing.Size(121, 21);
+            this.cmbTeacher.Size = new System.Drawing.Size(121, 24);
             this.cmbTeacher.TabIndex = 2;
             // 
             // numVariants
             // 
+            this.numVariants.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.numVariants.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.numVariants.Location = new System.Drawing.Point(545, 13);
             this.numVariants.Name = "numVariants";
-            this.numVariants.Size = new System.Drawing.Size(120, 20);
+            this.numVariants.Size = new System.Drawing.Size(120, 22);
             this.numVariants.TabIndex = 3;
             // 
             // btnLoadDisciplines
             // 
+            this.btnLoadDisciplines.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLoadDisciplines.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnLoadDisciplines.Location = new System.Drawing.Point(12, 72);
             this.btnLoadDisciplines.Name = "btnLoadDisciplines";
-            this.btnLoadDisciplines.Size = new System.Drawing.Size(110, 26);
+            this.btnLoadDisciplines.Size = new System.Drawing.Size(110, 30);
             this.btnLoadDisciplines.TabIndex = 4;
             this.btnLoadDisciplines.Text = "Загрузить план";
-            this.btnLoadDisciplines.UseVisualStyleBackColor = true;
+            this.btnLoadDisciplines.UseVisualStyleBackColor = false;
             this.btnLoadDisciplines.Click += new System.EventHandler(this.btnLoadDisciplines_Click);
             // 
             // btnLoadTeachers
             // 
-            this.btnLoadTeachers.Location = new System.Drawing.Point(146, 74);
+            this.btnLoadTeachers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLoadTeachers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLoadTeachers.Location = new System.Drawing.Point(128, 74);
             this.btnLoadTeachers.Name = "btnLoadTeachers";
-            this.btnLoadTeachers.Size = new System.Drawing.Size(112, 24);
+            this.btnLoadTeachers.Size = new System.Drawing.Size(145, 27);
             this.btnLoadTeachers.TabIndex = 5;
             this.btnLoadTeachers.Text = "Загрузить препод.";
-            this.btnLoadTeachers.UseVisualStyleBackColor = true;
+            this.btnLoadTeachers.UseVisualStyleBackColor = false;
             this.btnLoadTeachers.Click += new System.EventHandler(this.btnLoadTeachers_Click);
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(264, 72);
+            this.btnGenerate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGenerate.Location = new System.Drawing.Point(280, 73);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(96, 29);
             this.btnGenerate.TabIndex = 6;
             this.btnGenerate.Text = "Сгенерировать";
-            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.UseVisualStyleBackColor = false;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
             // btnSaveWord
             // 
+            this.btnSaveWord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSaveWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnSaveWord.Location = new System.Drawing.Point(382, 74);
             this.btnSaveWord.Name = "btnSaveWord";
             this.btnSaveWord.Size = new System.Drawing.Size(135, 27);
             this.btnSaveWord.TabIndex = 7;
             this.btnSaveWord.Text = "Сохранить в Word";
-            this.btnSaveWord.UseVisualStyleBackColor = true;
+            this.btnSaveWord.UseVisualStyleBackColor = false;
             this.btnSaveWord.Click += new System.EventHandler(this.btnSaveWord_Click);
             // 
             // btnOpenTeacherEditor
             // 
-            this.btnOpenTeacherEditor.Location = new System.Drawing.Point(535, 74);
+            this.btnOpenTeacherEditor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnOpenTeacherEditor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOpenTeacherEditor.Location = new System.Drawing.Point(523, 74);
             this.btnOpenTeacherEditor.Name = "btnOpenTeacherEditor";
-            this.btnOpenTeacherEditor.Size = new System.Drawing.Size(112, 27);
+            this.btnOpenTeacherEditor.Size = new System.Drawing.Size(136, 28);
             this.btnOpenTeacherEditor.TabIndex = 8;
             this.btnOpenTeacherEditor.Text = "редактор препод.";
-            this.btnOpenTeacherEditor.UseVisualStyleBackColor = true;
+            this.btnOpenTeacherEditor.UseVisualStyleBackColor = false;
             this.btnOpenTeacherEditor.Click += new System.EventHandler(this.btnOpenTeacherEditor_Click);
             // 
             // btnOpenGroupEditor
             // 
+            this.btnOpenGroupEditor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnOpenGroupEditor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnOpenGroupEditor.Location = new System.Drawing.Point(665, 77);
             this.btnOpenGroupEditor.Name = "btnOpenGroupEditor";
-            this.btnOpenGroupEditor.Size = new System.Drawing.Size(110, 24);
+            this.btnOpenGroupEditor.Size = new System.Drawing.Size(123, 24);
             this.btnOpenGroupEditor.TabIndex = 9;
             this.btnOpenGroupEditor.Text = "Редактор групп";
-            this.btnOpenGroupEditor.UseVisualStyleBackColor = true;
+            this.btnOpenGroupEditor.UseVisualStyleBackColor = false;
             this.btnOpenGroupEditor.Click += new System.EventHandler(this.btnOpenGroupEditor_Click);
             // 
             // dgvSchedule
             // 
+            this.dgvSchedule.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.dgvSchedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvSchedule.Location = new System.Drawing.Point(12, 126);
             this.dgvSchedule.Name = "dgvSchedule";
@@ -146,9 +167,11 @@
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(367, 383);
+            this.lblStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblStatus.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStatus.Location = new System.Drawing.Point(346, 376);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(42, 13);
+            this.lblStatus.Size = new System.Drawing.Size(96, 36);
             this.lblStatus.TabIndex = 11;
             this.lblStatus.Text = "Готово";
             // 
@@ -156,6 +179,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.dgvSchedule);
