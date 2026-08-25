575c69be46028380f0fe878eb2bd681e7bdc715d
Кира <Kirakuleschova@gmail.com>
2026-08-25 14:48:35 +0300

    Заполнила класс Учебная группа

--- ScheduleGenerator/Model/Group.cs
+++ ScheduleGenerator/Model/Group.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScheduleGenerator.Model
+﻿namespace ScheduleGenerator
 {
-    internal class Group
+    public class Group
     {
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public int Course { get; set; }
     }
 }
