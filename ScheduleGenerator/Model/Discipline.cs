ff3ea5897c6a247b546fef0e11bf4c4e5ec43202
Кира <Kirakuleschova@gmail.com>
2026-08-25 14:51:31 +0300

    Заполнила класс Дисциплина. но добавила туда ещё и курс, чтобы связывать с группой при запуске

--- ScheduleGenerator/Model/Discipline.cs
+++ ScheduleGenerator/Model/Discipline.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScheduleGenerator.Model
+﻿namespace ScheduleGenerator
 {
-    internal class Discipline
+    public class Discipline
     {
+        public string Name { get; set; }
+        public int HoursPerWeek { get; set; }
+        public string Teacher { get; set; } // ФИО преподавателя
+        public int Course { get; set; } // Добавила курс, чтобы связывать с группой при загрузке
     }
 }
